f4ac931366eb4b0befc768b5a4ccafc319fa00a0
Ian Stevens <iandevstevens@gmail.com>
2025-07-18 06:50:35 +0200

    a

--- src/app/non/page.tsx
+++ src/app/non/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <img alt="" src="/image.jpg" width={500} height={500}></img>
-      <img alt="" src="/image_big.png" width={500} height={500}></img>
+      <img alt="" src="/images/image.jpg" width={500} height={500}></img>
+      <img alt="" src="/images/image_big.png" width={500} height={500}></img>
       <Link href="/">home</Link>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Image alt="" src="/image.jpg" width={500} height={500}></Image>
-      <Image alt="" src="/image_big.png" width={500} height={500} quality={100}></Image>
+      <Image alt="" src="/images/image.jpg" width={500} height={500}></Image>
+      <Image alt="" src="/images/image_big.png" width={500} height={500} quality={100}></Image>
       <Link href="/non">non</Link>
     </div>
   );
