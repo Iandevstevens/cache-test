74594ab9506e1cd4156506667d58e4c9fc7ab07c
Ian Stevens <iandevstevens@gmail.com>
2025-07-18 06:45:23 +0200

    a

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/images/(.*)", // match any file in /images
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable", // 1 year
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/non/page.tsx
+++ src/app/non/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <img alt="" src="/image.jpg" width={100} height={100}></img>
-      <img alt="" src="/image_big.png" width={100} height={100}></img>
+      <img alt="" src="/image.jpg" width={500} height={500}></img>
+      <img alt="" src="/image_big.png" width={500} height={500}></img>
       <Link href="/">home</Link>
     </div>
   );
